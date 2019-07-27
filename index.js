3aa35d80329c0aec659e9f2cb5d7929b92ac77cd
Maian Tran <maiantran@Maian-Trans-MacBook-Pro.local>
2019-07-27 15:54:42 -0700

    prepare for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 
-const const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
